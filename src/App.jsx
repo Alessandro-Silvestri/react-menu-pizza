95b7348514a6193e94448f50936280c2a52f2bd8
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2026-03-07 18:31:37 +0000

    make gray the pizza not available

--- src/App.jsx
+++ src/App.jsx
@@ -61,9 +61,7 @@ function Menu() {
       <ul className="pizzas">
         {/* in react, using map, is mandatory using the internal property: "key" and indicate a property, in the object that is unique */}
         {pizzaData.map((pizza) => {
-          return !pizza.soldOut ? (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ) : null;
+          return <Pizza pizzaObj={pizza} key={pizza.name} />;
         })}
       </ul>
     </main>
@@ -71,16 +69,25 @@ function Menu() {
 }
 
 // best practice: passing an object as a props and using the property I need
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.photoName} />
+    // using the ternary operator for adding or removing css classes
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : null}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.photoName} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredient}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <p>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</p>
       </div>
     </li>
+  );
+}
+
+function Order({ closeHour }) {
+  return (
+    <div>
+      <p>We are open until {closeHour}:00. Come or visit us online</p>
+    </div>
   );
 }
 
@@ -90,11 +97,10 @@ function Footer() {
   const closeHour = 22;
   const isOpen = hour < closeHour && hour >= openHour;
   return (
+    // when a component gets too big you can extract and create another component
     <footer className="footer">
       {isOpen ? (
-        <div>
-          <p>We are open until {closeHour}:00. Come or visit us online</p>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We are closed. You can find us open between: {openHour}:00 and{" "}
